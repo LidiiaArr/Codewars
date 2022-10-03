8db7ead8bcc1192fe6003c5c24c28761145ab753
Lidiia A <100479453+LidiiaArr@users.noreply.github.com>
2022-10-03 13:57:41 +0300

    Switch it Up!

---
+++ 23.js
@@ -0,0 +1,7 @@
+//Switch it Up!
+//Приходит число и надо вернуть слово
+
+function switchItUp(number){
+    const o = ["Zero", "One", "Two", "Three","Four", "Five","Six", "Seven", "Eight", "Nine", "Ten"];
+    return o[number];
+    }
