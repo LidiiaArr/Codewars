02ba5d9ad015baf01e74aebd1ab7ef2f75b61507
Lidiia A <100479453+LidiiaArr@users.noreply.github.com>
2023-05-11 17:22:05 +0300

    Mumbling

---
+++ 70.js
@@ -0,0 +1,9 @@
+//Mumbling
+
+function accum(s) {
+    let res = "";
+    for(let i = 0; i < s.length ; i++){
+        res += s[i].toUpperCase() + s[i].toLowerCase().repeat(i) + (i < s.length - 1 ? "-": "");
+    }
+    return res;
+}
